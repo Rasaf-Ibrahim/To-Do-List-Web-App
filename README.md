06a8bcf816fbfcddfb4ef383679debefb790d85a
Rasaf-Ibrahim <rasaf.ibrahim25@gmail.com>
2021-04-10 09:52:07 +0600

    updated

---
+++ README.md
@@ -0,0 +1,14 @@
+# vue-firebase-crud-app
+This is a step by step tutorial that explains how to build a Vue.js 2+ CRUD web application using Firebase database.
+
+[Vue.js 2 + Firebase: Build Vue CRUD App with Cloud Firestore](https://www.positronx.io/vue-js-firebase-build-vue-crud-app-with-cloud-firestore/)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark justify-content-between flex-nowrap flex-row">
+      <div class="container">
+        <a class="navbar-brand float-left">Firebase Vue CRUD Example</a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/">Add User</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/list">View Users</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
